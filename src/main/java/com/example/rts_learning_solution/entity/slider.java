5d9464fbeda5fd49e8ec225d22c8660cfac52874
sang1999alhp0393463756 <sangnvhe130388@fpt.edu.vn>
2021-05-20 13:46:54 +0700

    small modify

--- src/main/java/com/example/rts_learning_solution/entity/slider.java
+++ src/main/java/com/example/rts_learning_solution/entity/slider.java
@@ -13,4 +13,52 @@ public class slider {
     private String backLink;
     private String status;
     private String notes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getImageLink() {
+        return imageLink;
+    }
+
+    public void setImageLink(String imageLink) {
+        this.imageLink = imageLink;
+    }
+
+    public String getBackLink() {
+        return backLink;
+    }
+
+    public void setBackLink(String backLink) {
+        this.backLink = backLink;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
 }
